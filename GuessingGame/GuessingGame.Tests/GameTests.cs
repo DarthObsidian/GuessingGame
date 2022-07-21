f0835cfe5851a3edcf74154c65e6a4735312b8e3
MatthewMeyers <jedimastermeyers@gmail.com>
2022-07-21 17:14:54 -0600

    Added/Configured Unit tests
    Added the unit test project. Configured the game project so internal items are visible to the unit tests

---
+++ GuessingGame/GuessingGame.Tests/GameTests.cs
@@ -0,0 +1,19 @@
+namespace GuessingGame.Tests
+{
+    public class GameTests
+    {
+        Game game;
+
+        [SetUp]
+        public void Setup()
+        {
+            game = new();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
